7f0cf3ad1c0619b52e76457937bd73b751dad3bd
Patrik Hegethorn <patrik.hegethorn@avanade.com>
2017-06-01 11:22:27 +0200

    Test with AD in view

--- Awards/Content/app.js
+++ Awards/Content/app.js
@@ -1,19 +1,59 @@
 ﻿$(document).on('click', function (e) {
-    if ($(e.target).closest('nav').length < 1 ) {
+    if ($(e.target).closest('nav').length < 1) {
         $('#main-menu').collapse('hide');
     }
 });
 
-angular.module('AvaSummerAwards', [])
-    .controller('MainController', ['VoteService', '$interval', function (VoteService, $interval) {
-        var ctrl = this;
-        ctrl.data = VoteService.data;
+angular.module('AvaSummerAwards', ['ngRoute', 'AdalAngular'])
+    .config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider',
+        function ($routeProvider, $httpProvider, adalProvider) {
+            $routeProvider.when("/Home",
+                {
+                    controller: "MainController",
+                    template: "<div>hej</div>"
+                })
+                .when("/App",
+                {
+                    controller: "AppController",
+                    templateUrl: "/App",
+                    requireADLogin: true,
+                })
+                .otherwise({ redirectTo: "/Home" });
+            var endpoints = {
+                // Map the location of a request to an API to a the identifier of the associated resource
+                //url:apiResourceId
+                "https://localhost:44375/":
+                    "https://marcusengelhartavanade.onmicrosoft.com/AvaSummerAwards"
+            };
 
-        ctrl.vote = VoteService.vote;
-        ctrl.removeVote = VoteService.removeVote;
+            adalProvider.init(
+                {
+                    instance: 'https://login.microsoftonline.com/',
+                    tenant: 'common', //for multi-tenant
+                    clientId: '884c99db-e371-4707-be34-56b1c064db0f', //copy from Azure active directory application for this client
+                    extraQueryParameter: '',
+                    endpoints: endpoints
+                    //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+                },
+                $httpProvider
+            );
+        }])
+    .controller('AppController', ['VoteService', '$interval', '$scope', '$rootScope', 'adalAuthenticationService', function (VoteService, $interval, $scope, $rootScope, adalService) {
+        console.log($rootScope.userInfo)
+        $scope.data = VoteService.data;
 
+        $scope.vote = VoteService.vote;
+        $scope.removeVote = VoteService.removeVote;
+
+    }]).controller('MainController', ['$scope', '$rootScope', 'adalAuthenticationService', function ($scope, $rootScope, adalService) {
+        $scope.logout = function () {
+            adalService.logOut();
+        };
+        $scope.login = function () {
+            adalService.login();
+            console.log($rootScope.userInfo)
+        };
     }])
-
     .factory('VoteService', ['$http', function ($http) {
         var data = {
             user: 'kalkon'
